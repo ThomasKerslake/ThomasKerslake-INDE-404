d33caee34b86374174991d4c446d9622fe39d399
(s) Thomas Kerslake <tkerslake@pm7-0101.uopnet.plymouth.ac.uk>
2018-02-02 13:56:29 +0000

    Edit to clock class work

--- Session 3 - ES6 and the DOM/JS Drum kit - Start/scripts/script.js
+++ Session 3 - ES6 and the DOM/JS Drum kit - Start/scripts/script.js
@@ -1,4 +1,18 @@
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e){
+  if(e.proertyName !== 'transform') return;
+  console.log(e.);
+  this.classlist.remove('playing');
+}
+
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio)return;
+  audio.CurrentTime = 0;
+  audio.play();
+  key.classlist.add('playing');
+
 })
--- Session 3 - ES6 and the DOM/JS and CSS Clock - Start/scripts/scripts.js
+++ Session 3 - ES6 and the DOM/JS and CSS Clock - Start/scripts/scripts.js
@@ -1,1 +1,11 @@
-console.log("hi");
+
+const secondHand = document.querySelector('.second-hand');
+
+function setDate(){
+const now = new Date();
+const seconds = now.getSeconds();
+const secondsDegree = ((seconds / 60) * 360) + 90;
+secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
